74ebac3299b5ec6b4374acb6f30cb8b0110606d6
sebastien doyez <doyez.sebastien34090@gmail.com>
2026-08-18 21:26:43 -0400

    improove the tools

--- BackEnd/tools/ambiance.py
+++ BackEnd/tools/ambiance.py
@@ -1,7 +1,7 @@
 from langchain_core.tools import tool
 
 @tool
-def set_musique(style="default", volume=50):
+def set_music(style="default", volume=50):
     """
     Function to set the music volume.
     """
--- BackEnd/tools/test_plant_watering.py
+++ BackEnd/tools/test_plant_watering.py
@@ -1,5 +1,5 @@
 from plant_watering import plant_watering
 
 def test_plant_watering():
-    result = plant_watering()
+    result = plant_watering.invoke({})
     assert result is True
