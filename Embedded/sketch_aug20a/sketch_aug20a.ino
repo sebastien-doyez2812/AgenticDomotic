53d8bc05e52a34932e6ec6fec3596f483713b586
sebastien doyez <doyez.sebastien34090@gmail.com>
2026-08-22 22:14:31 -0400

    Code to connect to wifi

--- Embedded/sketch_aug20a/sketch_aug20a.ino
+++ Embedded/sketch_aug20a/sketch_aug20a.ino
@@ -1,41 +1,30 @@
-#include <ArduinoWiFiServer.h>
-#include <BearSSLHelpers.h>
-#include <CertStoreBearSSL.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266WiFiAP.h>
-#include <ESP8266WiFiGeneric.h>
-#include <ESP8266WiFiGratuitous.h>
-#include <ESP8266WiFiMulti.h>
-#include <ESP8266WiFiSTA.h>
-#include <ESP8266WiFiScan.h>
-#include <ESP8266WiFiType.h>
-#include <WiFiClient.h>
-#include <WiFiClientSecure.h>
-#include <WiFiClientSecureBearSSL.h>
-#include <WiFiServer.h>
-#include <WiFiServerSecure.h>
-#include <WiFiServerSecureBearSSL.h>
-#include <WiFiUdp.h>
-
 #include <ESP8266WiFi.h>
 #include <SoftwareSerial.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-const char* ssid = "YOUR_SSID_HERE"
-const char* pwd  = "YOUR_PASSWORD_HERE"
+const char* ssid = "YOUR_SSID";
+const char* pwd  = "YOUR_PWD ";
 
 WiFiServer server(1234);
 LiquidCrystal_I2C lcd(0x20, 16, 2);
 
 void setup() {
-// Config WIFI:
-WiFi.mode(WIFI_STA);
-WiFi.begin(ssid, pwd);
-while(WiFi.status() != WL_CONNECTED)
-{
-  Serial.println(".");
-}
+  Serial.begin(115200);
+  delay(20);
+  Serial.println("Try to connect to Wifi...");
+
+  // Config WIFI:
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pwd);
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println(".");
+  }
+  Serial.println("Conected!");
+  Serial.print("Adresse IP : ");
+  Serial.println(WiFi.localIP());
+
 // Config I2C:
 }
 
