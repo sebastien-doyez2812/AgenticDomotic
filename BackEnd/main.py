818fdb4e625896b3a23d2e1a5493abc1a66d0100
sebastien doyez <doyez.sebastien34090@gmail.com>
2026-08-26 22:44:29 -0400

    Add a way to make the model speak

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,23 +1,23 @@
 from langchain_ollama import ChatOllama
 from langgraph.graph import END, START, StateGraph
 from langgraph.prebuilt import ToolNode, tools_condition
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import AIMessage, HumanMessage
 from tools.ambiance import set_music
 from tools.plant_watering import plant_watering
 from state import State
 from prompt.system import SYSTEM_PROMPT
 import speech_recognition as sr
-
+import pyttsx3
 
 
 def agent_factory(custom_model = None):
     tools = [set_music, plant_watering]
-    model = custom_model or ChatOllama(model="gemma4:latest ").bind_tools(tools)
+    model = custom_model or ChatOllama(model="gemma4:latest").bind_tools(tools)
 
     def call_model(state: State):
         messages = [SYSTEM_PROMPT] + state["graph_state"]
         response = model.invoke(messages)
-        return {"graph_state": state["graph_state"] + [response]}
+        return {"graph_state": [response]}
 
     workflow = StateGraph(State)
     workflow.add_node("agent", call_model)
@@ -35,24 +35,17 @@ def agent_factory(custom_model = None):
     app = workflow.compile()
     return app
 
-# Test the plant_watering function with the agent workflow:
-# if __name__ == "__main__":
-#     inputs = {
-#     "graph_state": [
-#         HumanMessage(content="Allume la musique jazz avec un volume de 5")
-#     ]
-#     }
-#     print("--- Début de l'exécution ---")
-#     # app.invoke(inputs)
-#     for event in app.stream(inputs):
-#         for node_name, node_output in event.items():
-#             print(f"\n[Nœud exécuté : {node_name}]")
-#             print("Sortie :", node_output)
-#     print("\n--- Fin de l'exécution ---")
-
+def make_agent_speak(engine, conversation_state):
+    if conversation_state["graph_state"]:
+        last_response = conversation_state["graph_state"][-1]
+        if isinstance(last_response, AIMessage):
+            print("Agent response:", last_response.content)
+            engine.say(last_response.content)
+            engine.runAndWait()
 
 if __name__ == "__main__":
     app =  agent_factory()
+    engine = pyttsx3.init()
     r = sr.Recognizer()
 
     conversation_state = {"graph_state": []}
@@ -74,12 +67,18 @@ if __name__ == "__main__":
                 ]
             }
             print("--- Beginning of execution ---")
-            for event in app.stream(inputs):
+
+            config = {"recursive_limit": 3, "max_iterations": 2}
+            for event in app.stream(inputs, config= config):
                 for node_name, node_output in event.items():
-                    print(f"\n[Node executed : {node_name}]")
-                    print("Output :", node_output)
-                    conversation_state = node_output  # Update the conversation state with the latest output
+                    # print(f"\n[Node executed : {node_name}]")
+                    # print("Output :", node_output)
+                    if "graph_state" in node_output:
+                        conversation_state = node_output  # Update the conversation state with the latest output
+                          # Update the conversation state with the latest output
             print("\n--- End of execution ---")
+
+            make_agent_speak(engine, conversation_state)
 
         except sr.UnknownValueError:
             print("Google Speech Recognition could not understand the audio.")
