a98de8e9b00a82667971ad4927ce2ddc902e0ba5
sebastien doyez <doyez.sebastien34090@gmail.com>
2026-08-26 22:00:59 -0400

    Add Speech recognition

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -6,6 +6,8 @@ from tools.ambiance import set_music
 from tools.plant_watering import plant_watering
 from state import State
 from prompt.system import SYSTEM_PROMPT
+import speech_recognition as sr
+
 
 
 def agent_factory(custom_model = None):
@@ -33,18 +35,56 @@ def agent_factory(custom_model = None):
     app = workflow.compile()
     return app
 
-# To test the back end in real life:
+# Test the plant_watering function with the agent workflow:
+# if __name__ == "__main__":
+#     inputs = {
+#     "graph_state": [
+#         HumanMessage(content="Allume la musique jazz avec un volume de 5")
+#     ]
+#     }
+#     print("--- Début de l'exécution ---")
+#     # app.invoke(inputs)
+#     for event in app.stream(inputs):
+#         for node_name, node_output in event.items():
+#             print(f"\n[Nœud exécuté : {node_name}]")
+#             print("Sortie :", node_output)
+#     print("\n--- Fin de l'exécution ---")
+
+
 if __name__ == "__main__":
-    app = agent_factory()
-    inputs = {
-    "graph_state": [
-        HumanMessage(content="Pourrais-tu arroser les plantes s'il te plaît ?")
-    ]
-    }
-    print("--- Beginning of the execution: ---")
-    # app.invoke(inputs)
-    for event in app.stream(inputs):
-        for node_name, node_output in event.items():
-            print(f"\n[Node executed : {node_name}]")
-            print("Output :", node_output)
-    print("\n--- End of execution ---")
+    app =  agent_factory()
+    r = sr.Recognizer()
+
+    conversation_state = {"graph_state": []}
+
+    while(True):
+        
+        with sr.Microphone() as source:
+            print("Agent is listening...")
+            r.adjust_for_ambient_noise(source)
+            audio = r.listen(source)
+
+        try:
+            user_input = r.recognize_google(audio, language="fr-FR")
+            print("You said :", user_input)
+
+            inputs = {
+                "graph_state": [
+                    HumanMessage(content=user_input)
+                ]
+            }
+            print("--- Beginning of execution ---")
+            for event in app.stream(inputs):
+                for node_name, node_output in event.items():
+                    print(f"\n[Node executed : {node_name}]")
+                    print("Output :", node_output)
+                    conversation_state = node_output  # Update the conversation state with the latest output
+            print("\n--- End of execution ---")
+
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand the audio.")
+        except sr.RequestError as e:
+            print(f"Error occurred while requesting results from Google Speech Recognition; {e}")
+        except KeyboardInterrupt:
+            print("Exiting the program.")
+            break
